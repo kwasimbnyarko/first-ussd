71d639367ea55cffe9e54aaeda36bb781abf6dae
Biritwum-Nyarko Kwasi Manu <51910665+kwasimbnyarko@users.noreply.github.com>
2022-11-30 13:54:05 +0000

    post finally working after I imported body-parser

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = express.Router();
+const jsonParser = bodyParser.json();
 const app = express();
 
 
@@ -8,12 +8,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/getData',function(req, res){
+app.post('/getData',function(req, res){
+    console.log(req.body["test"]);
     res.status(200).send({
         success: "true",
         name:"Deez",
-        response:"waguan"
+        "response":req.body["test"]
     });
+
 })
 
 const PORT = 5111;
