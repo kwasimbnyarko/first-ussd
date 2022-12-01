20ae4e52a1d668cea0ec21c662fee9acd60c2d6c
Biritwum-Nyarko Kwasi Manu <51910665+kwasimbnyarko@users.noreply.github.com>
2022-12-01 21:49:17 +0000

    main.js works as a normal ussd. direct ussd still  under construction

--- direct-ussd.js
+++ direct-ussd.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let page_number;
-
 let sessionTracker = new Map();
 let userState;
 let userStateReason;
@@ -37,6 +35,7 @@ function firstChoice (choice){
             break;
     }
     console.log(`choice 1 = ${userState}`);
+
 }
 
 function secondChoice (choice)
@@ -62,19 +61,61 @@ app.post('*',function(req, res){
     let msisdn = req.body["MSISDN"];
     let user_id = req.body["USERID"];
     let message_type;
-    // let session_id = req.body["SESSIONID"];
+    let session_id = req.body["SESSIONID"];
     let user_data = req.body["USERDATA"];
+    console.log(`list before ${Object.fromEntries(sessionTracker)}`);
+    let page_number = sessionTracker.get(user_data);
+    console.log(`list after ${Object.fromEntries(sessionTracker)}`);
+
 
 
     if(req.body["MSGTYPE"] === true){
 
+        if (user_data.length > 9){
+            let direct_code = user_data.slice(9,user_data.length);
+            console.log(direct_code);
+            for (let i =  0;i < direct_code.length; i++){
+                if(direct_code[i]==="*"){
+                    console.log("here");
+                    continue;
+                }else{
+                    if(page_number===undefined){
+                        console.log(`first run pn: ${page_number}`);
+                        firstChoice(direct_code[i]);
+                        page_number = 1;
+                        console.log(`pn increased to ${page_number}`);
+                        sessionTracker.set(session_id,page_number);
+
+                    }else if(page_number===1){
+                        secondChoice(direct_code[i]);
+                        console.log(`second run pn: ${page_number}`);
+                        page_number+=1;
+                        console.log(`pn increased to ${page_number}`);
+                        sessionTracker.set(session_id,page_number);
+                    }
+                }
+            }
+            message_type = page_number !== 2;
+            console.log("MT:" + message_type);
+            switch(page_number){
+                case 1:
+                    message = `Why are you ${userState}?\n Is it because of\n1. ${option_a}\n2. ${option_b}\n3. ${option_c}`;
+                    break;
+                case 2:
+                    message = `You are ${userState} because of ${userStateReason}`;
+                    break;
+            }
+            console.log("MN:" + page_number);
+
+        }else{
+
             message = `Welcome to the base ;)\nThis is USSDTEST's USSD.\nHow are you feeling?\n1. ${option_1}\n2. ${option_2}\n3. ${option_3}`;
             message_type = true;
-            // console.log(`Old page number first ${page_number}`);
+            console.log(`Old page number first ${page_number}`);
             page_number = 1;
-            // console.log(`New page number first ${page_number}`);
-            // sessionTracker.set(session_id,page_number);
-
+            console.log(`New page number first ${page_number}`);
+            sessionTracker.set(session_id,page_number);
+        }
 
     }else if(page_number === 1){
         console.log("hola");
@@ -82,10 +123,10 @@ app.post('*',function(req, res){
         firstChoice(user_data);
         message = `Why are you ${userState}?\n Is it because of\n1. ${option_a}\n2. ${option_b}\n3. ${option_c}`;
         message_type = true;
-        // console.log(`Old page number ${page_number}`);
+        console.log(`Old page number ${page_number}`);
         page_number += 1;
-        // console.log(`New page number ${page_number}`);
-        // sessionTracker.set(session_id,page_number);
+        console.log(`New page number ${page_number}`);
+        sessionTracker.set(session_id,page_number);
 
     }else if(page_number === 2){
         console.log("amigo");
@@ -93,10 +134,10 @@ app.post('*',function(req, res){
         secondChoice(user_data);
         message = `You are ${userState} because of ${userStateReason}`;
         message_type = false;
-        // console.log(`Old page number ${page_number}`);
-        // page_number += 1;
-        // console.log(`New page number ${page_number}`);
-        // sessionTracker.set(session_id,page_number);
+        console.log(`Old page number ${page_number}`);
+        page_number += 1;
+        console.log(`New page number ${page_number}`);
+        sessionTracker.set(session_id,page_number);
     }
 
     console.log({
